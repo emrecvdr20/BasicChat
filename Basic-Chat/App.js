47814735f0931b5e60f131826edcc014271a9060
Emre <64108864+emrecvdr20@users.noreply.github.com>
2022-09-19 14:50:39 +0300

    chat

---
+++ Basic-Chat/App.js
@@ -0,0 +1,99 @@
+import React, { useEffect } from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ChatList from "./screens/ChatList";
+import Settings from "./screens/Settings";
+import Chat from "./screens/Chat";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Provider, DefaultTheme } from "react-native-paper";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8Ecjpi1MO8cVEpTTpn8xKbX_HvXpPO9o",
+  authDomain: "chat-app-ed081.firebaseapp.com",
+  projectId: "chat-app-ed081",
+  storageBucket: "chat-app-ed081.appspot.com",
+  messagingSenderId: "1080709948382",
+  appId: "1:1080709948382:web:8c31cf73dd1b0d76cbf34f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const Stack = createNativeStackNavigator();
+
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        navigation.navigate("SignUp");
+      }
+    });
+  }, []);
+
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({color, size }) => {
+          return (
+            <Ionicons
+              name={route.name === "ChatList" ? "chatbubbles" : "settings"}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tabs.Screen name="ChatList" component={ChatList} />
+      <Tabs.Screen name="Settings" component={Settings} />
+    </Tabs.Navigator>
+  );
+};
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#2196f3",
+    accent: "#e91e63",
+  },
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Provider theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={TabsNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Chat" component={Chat} />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ presentation: "fullScreenModal" }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ presentation: "fullScreenModal" }}
+          />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
+};
+
+
+export default App;
